731d0b4c62282782232656a1ecf23d1eb0437440
Eddie Qi <eddieqi128@gmail.com>
2018-03-23 18:59:42 +1100

    Rapid-application development (RAD) is both a general term, used to refer to adaptive software development approaches, as well as the name for James Martin's approach to rapid development. In general, RAD approaches to software development put less emphasis on planning and more emphasis on an adaptive process. Prototypes are often used in addition to or sometimes even in place of design specifications.
    RAD is especially well suited for (although not limited to) developing software that is driven by user interface requirements. Graphical user interface builders are often called rapid application development tools. Other approaches to rapid development include the adaptive, agile, spiral, and unified models.

--- app/src/main/java/com/nutcorp/nutcontrol/MainActivity.java
+++ app/src/main/java/com/nutcorp/nutcontrol/MainActivity.java
@@ -2,8 +2,12 @@ package com.nutcorp.nutcontrol;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.icu.util.Output;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +15,27 @@ import android.widget.PopupMenu;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     final private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+    final private HashMap<MenuItem,String> deviceMACs = new HashMap<>();
+    final private UUID SPPUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private InputStream inStream = null;
+    private OutputStream outStream = null;
+    private BluetoothSocket deviceSocket;
+
+    private Button butt;
+    private TextView valText;
+    private boolean isConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SeekBar mainSeek = findViewById(R.id.seekBar);
-        Button butt = findViewById(R.id.connectButton);
-        final TextView valText = findViewById(R.id.valueText);
+        butt = findViewById(R.id.connectButton);
+        valText = findViewById(R.id.valueText);
 
         final PopupMenu deviceMenu = new PopupMenu(MainActivity.this, butt);
 
@@ -31,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar changedSeek, int val, boolean idk) {
                 valText.setText(String.valueOf(val));
+                if (isConnected) {
+                    //Log.d("idk", Integer.toString(val));
+                    try {
+                        outStream.write((Integer.toString(val) + ";").getBytes());
+                    } catch (IOException e) {
+                        Log.e("idk", "Write failed", e);
+                    }
+                }
             }
 
             @Override
@@ -47,28 +76,97 @@ public class MainActivity extends AppCompatActivity {
         butt.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View fromView) {
-                populateMenu(deviceMenu);
-                deviceMenu.show();
+                if (!isConnected) {
+                    populateMenu(deviceMenu);
+                    deviceMenu.show();
+                } else {
+                    deviceMenu.getMenu().clear();
+                    deviceMenu.getMenu().add(getString(R.string.disconnect));
+                    deviceMenu.show();
+                }
             }
         });
 
         deviceMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getTitle() == getString(R.string.disconnect)) {
+                    disconnect();
+                } else {
+                    butt.setText(getString(R.string.connecting, menuItem.getTitle()));
+                    butt.setEnabled(false);
+                    connectToDevice(deviceMACs.get(menuItem));
+                }
 
                 return true;
             }
         });
     }
 
+    private void connectToDevice(String macToConnect) {
+        BluetoothDevice device = btAdapter.getRemoteDevice(macToConnect);
+        BluetoothSocket temp = null;
+        try {
+            temp = device.createRfcommSocketToServiceRecord(SPPUUID);
+        } catch (IOException e) {
+            disconnected();
+        }
+        deviceSocket = temp;
+        Log.d("idk", "Socket created");
+
+        try {
+            deviceSocket.connect();
+            connected(macToConnect);
+        } catch (IOException e) {
+            Log.e("idk", "Connect failed", e);
+            disconnect();
+        }
+
+        // Open sockets
+        InputStream inTemp = null;
+        OutputStream outTemp = null;
+        try {
+            inTemp = deviceSocket.getInputStream();
+            outTemp = deviceSocket.getOutputStream();
+        } catch (IOException e) {
+            disconnect();
+        }
+        inStream = inTemp;
+        outStream = outTemp;
+    }
+
+    private void disconnect() {
+        try {
+            deviceSocket.close();
+        } catch (IOException e) {}
+        deviceSocket = null;
+        disconnected();
+    }
+
     private void populateMenu(PopupMenu menu) {
         if (btAdapter == null) return;
         if (!btAdapter.isEnabled()) return; // TODO
 
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
         menu.getMenu().clear();
+        deviceMACs.clear();
+
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
         for (BluetoothDevice device : pairedDevices) {
-            menu.getMenu().add(device.getName() + " (" + device.getAddress() + ")");
+            deviceMACs.put(menu.getMenu().add(device.getName() + " (" + device.getAddress() + ")"),
+                    device.getAddress());
         }
+    }
+
+    private void connected(String deviceMAC) {
+        isConnected = true;
+        butt.setEnabled(true);
+        butt.setText(getString(R.string.connected, deviceMAC));
+    }
+
+    private void disconnected() {
+        isConnected = false;
+        butt.setEnabled(true);
+        butt.setText(getString(R.string.dc_text));
     }
 }
